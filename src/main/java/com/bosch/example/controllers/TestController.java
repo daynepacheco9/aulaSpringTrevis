68ff2576ec1de897425ec4509bd109a07bdc221d
Dayne Pacheco <daynepacheco@gmail.com>
2024-06-07 16:51:59 -0300

    não terminei

--- src/main/java/com/bosch/example/controllers/TestController.java
+++ src/main/java/com/bosch/example/controllers/TestController.java
@@ -1,13 +1,34 @@
 package com.bosch.example.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.bosch.example.dto.User;
 
 @RestController
 public class TestController {
     
     @GetMapping("/test")
-    public Integer test(Integer a, Integer b) {
-        return a + b;
+    public String test(@RequestBody User user) {
+        if (user.name()== null || user.name().isEmpty()) {
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST, "name is missing"
+            );
+        }else if (user.email() == null || user.email().isEmpty()) {
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST, "email is missing"
+            );
+        }else if (user.password() == null || user.password().isEmpty()) {
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST, "password is missing"
+            );
+        }
+
+        return user.name();
     }
 }
